8e41287a22ae08f7856563fca6a1299ca5c6b659
JoseAnd10 <jamado567@gmail.com>
2024-12-10 15:36:35 +0100

    Contenido inicial

---
+++ src/main/java/com/mycompany/creacion_y_conexion_de_repositorio/Creacion_y_conexion_de_repositorio.java
@@ -0,0 +1,8 @@
+package com.mycompany.creacion_y_conexion_de_repositorio;
+
+public class Creacion_y_conexion_de_repositorio {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
